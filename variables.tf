9367baff0128b72d11064c00ff8b3ec7d1704dfd
JS Park <jsp@hashicorp.com>
2021-01-18 08:41:56 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "core" {
+  default = {
+    org        = "skhynix"
+    workspace  = "misO_mgmt_workspace"
+    user_token = "yqdVq1ptEmz1kw.atlasv1.0q2YUO5KgjNCVYBNTzIf3zI5IDMtBAIY2TzkuGzGNyz0C7bkaiaPFFBVm3YIlhZscUs"
+  }
+}
